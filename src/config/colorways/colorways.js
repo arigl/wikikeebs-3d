b173ab98287a1893cc5ceb013fbc5f9282b07aa1
Alex Rigl <crigl@chapman.edu>
2024-07-24 23:52:34 -0700

    new favicon title and removed colorway

--- src/config/colorways/colorways.js
+++ src/config/colorways/colorways.js
@@ -70,7 +70,6 @@ import colorway_nuclear_data from "./colorway_nuclear_data.json";
 import colorway_finer_things from "./colorway_finer_things.json";
 import colorway_gregory from "./colorway_gregory.json";
 import colorway_pluto from "./colorway_pluto.json";
-import colorway_dragonwitch from "./colorway_dragonwitch.json";
 
 const COLORWAYS = {
   //APPEND
@@ -79,7 +78,6 @@ const COLORWAYS = {
   night_sakura: colorway_night_sakura,
   blacklight: colorway_blacklight,
   botanical: colorway_botanical,
-  dragon_witch: colorway_dragonwitch,
   amalfi: colorway_amalfi,
   wob: colorway_wob,
   bow: colorway_bow,
